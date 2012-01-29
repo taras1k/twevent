cf32c2ed93bd82c7623e63a385ec6e320020ba49
Taras Nek <mr.taras1k@gmail.com>
2012-01-29 22:49:35 +0200

    titter loging template tags

--- twevent/urls.py
+++ twevent/urls.py
@@ -1,5 +1,5 @@
 from django.conf.urls.defaults import patterns, include, url
-
+from django.conf import settings
 # Uncomment the next two lines to enable the admin:
 # from django.contrib import admin
 # admin.autodiscover()
@@ -9,7 +9,8 @@ urlpatterns = patterns('',
     url(r'^login/?$', 'users.views.twitter_login'),
     url(r'^logout/?$', 'users.views.twitter_logout'),
     url(r'^login/authenticated/?$', 'users.views.twitter_authenticated'),
-   
+    url(r'^static/(?P<path>.*)$', 'django.views.static.serve',
+        {'document_root': settings.MEDIA_ROOT, 'show_indexes':True}),
     # Examples:
     # url(r'^$', 'twevent.views.home', name='home'),
     # url(r'^twevent/', include('twevent.foo.urls')),
